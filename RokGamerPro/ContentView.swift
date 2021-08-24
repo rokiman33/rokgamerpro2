6d62121577ed7ac8243a2cb2e63ebf21e82a4fdc
rdo <richarddiaz.esfera@gmail.com>
2021-08-23 23:19:32 -0400

    Initial Commit

---
+++ RokGamerPro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RokGamerPro
+//
+//  Created by Richard Diaz Orellana on 23-08-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RokGamerPro/RokGamerProApp.swift
@@ -0,0 +1,17 @@
+//
+//  RokGamerProApp.swift
+//  RokGamerPro
+//
+//  Created by Richard Diaz Orellana on 23-08-21.
+//
+
+import SwiftUI
+
+@main
+struct RokGamerProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
