e2e02135eceb7ecd0560e4788f066e3c031cb4ee
rdo <richarddiaz.esfera@gmail.com>
2021-09-25 22:53:18 -0300

    version1

--- RokGamerPro/ContentView.swift
+++ RokGamerPro/ContentView.swift
@@ -9,9 +9,201 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                Spacer()
+                Color(red: 29/255, green: 30/255, blue: 53/255, opacity: 1.0).ignoresSafeArea()
+                VStack{
+                    Image("ROKGamerSmall").resizable().aspectRatio(contentMode: .fit ).frame(width:237).padding(.bottom, 42)
+                InicioYRegistroView()
+                }
+               
+            }.navigationBarHidden(true)
+        }
     }
+}
+
+struct InicioYRegistroView:View{
+    
+    @State var tipoInicioSesion = true
+    var body: some View{
+        VStack{
+            HStack {
+                Spacer()
+                Button("INICIO SESION"){
+                    tipoInicioSesion = true
+                }.foregroundColor(tipoInicioSesion ? .white: .gray)
+                Spacer()
+                Button("REGISTRATE"){
+                    tipoInicioSesion = false
+                }.foregroundColor(tipoInicioSesion ? .gray: .white)
+                Spacer()
+            }
+            Spacer(minLength: 42)
+            if tipoInicioSesion == true {
+                InicioSesionView()
+            }else{
+                RegistroView()
+            }
+            
+            
+        }
+    }
+    
+}
+
+struct InicioSesionView:View {
+    @State var correo = ""
+    @State var contraseña = ""
+    @State var IsPantallaHomeActive = false
+    
+    
+    var body: some View{
+        ScrollView {
+            VStack(alignment:.leading) {
+                Text("Correo Electronico").foregroundColor(Color("Dark-Cian"))
+                ZStack(alignment:.leading){
+                    if correo.isEmpty{
+                        Text("ejemplo@gmail.com").font(.caption).foregroundColor(Color("Light-Gray"))
+                    }
+                    TextField("", text: $correo).foregroundColor(.white)
+                }
+                Divider().frame(height:1).background(Color("Dark-Cian")).padding(.bottom)
+                Text("Contraseña").foregroundColor(Color("Dark-Cian"))
+                ZStack(alignment:.leading){
+                    if contraseña.isEmpty{
+                        Text("Escribe tu Contraseña").font(.caption).foregroundColor(Color("Light-Gray"))
+                    }
+                    SecureField("", text: $contraseña).foregroundColor(.white)
+                }
+                Divider().frame(height:1).background(Color("Dark-Cian")).padding(.bottom)
+                
+                Text("¿Olvidaste tu Contraseña?").font(.footnote).frame(width: 300, alignment:.trailing).foregroundColor(Color("Dark-Cian")).padding(.bottom)
+                
+                Button(action: IniciarSesion, label: {
+                    Text("Iniciar Sesion").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white)
+                        .frame(maxWidth:.infinity,alignment: .center).padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18)).overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color("Dark-Cian"),lineWidth: 1).shadow(color: .white,radius: 1))
+                })
+                
+                Text("Iniciar Sesion con Redes Sociales").foregroundColor(.white)
+                HStack{
+                    Button(action: {print("Inicio de sesion con Facebook")}, label: {
+                        Text("Facebook").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                            .frame(maxWidth:.infinity,alignment: .center).background(Color("Blue-Gray"))
+                            .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                    })
+                    .padding()
+                    Button(action: {print("Inicio de sesion con Twitter")}, label: {
+                        Text("Twitter").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                            .frame(maxWidth:.infinity,alignment: .center).background(Color("Blue-Gray"))
+                            .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                    })
+                }
+            }.padding(.horizontal,77.0)
+            
+            NavigationLink(
+                destination: Home(),
+                isActive: $IsPantallaHomeActive,
+                label: { EmptyView()
+                           })
+        }
+    }
+}
+
+func IniciarSesion() {
+   print("Inicio de Sesion")
+}
+
+
+struct RegistroView:View {
+    @State var correo = ""
+     @State var contraseña = ""
+    @State var confirmarcontraseña = ""
+    
+    var body: some View{
+        ScrollView{
+            VStack(alignment:.center){
+                Text("Elija una Foto de Perfil").foregroundColor(.white).fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                Text("Puedes Cambiar o Elegir mas Adelante").font(.footnote).fontWeight(.light).foregroundColor(.gray).padding(.bottom)
+                
+                Button(action: TomarFoto, label: {
+                    ZStack {
+                        Image("profile").resizable().aspectRatio(contentMode: .fit).frame(width: 80, height: 80)
+                        Image(systemName: "camera").foregroundColor(.white)
+                        
+                    }
+                }).padding(.bottom)
+                
+                
+            }
+            
+            VStack(alignment:.leading){
+            VStack(alignment:.leading) {
+                Text("Correo Electronico*").foregroundColor(Color("Dark-Cian"))
+                ZStack(alignment:.leading){
+                    if correo.isEmpty{
+                        Text("ejemplo@gmail.com").font(.caption).foregroundColor(Color("Light-Gray"))
+                    }
+                    TextField("", text: $correo).foregroundColor(.white)
+                }
+                Divider().frame(height:1).background(Color("Dark-Cian")).padding(.bottom)
+                Text("Contraseña*").foregroundColor(Color("Dark-Cian"))
+                ZStack(alignment:.leading){
+                    if contraseña.isEmpty{
+                        Text("Escribe tu Contraseña").font(.caption).foregroundColor(Color("Light-Gray"))
+                    }
+                    SecureField("", text: $contraseña).foregroundColor(.white)
+                }
+                Divider().frame(height:1).background(Color("Dark-Cian")).padding(.bottom)
+                Text("Confirmar Contraseña*").foregroundColor(Color("Dark-Cian"))
+                ZStack(alignment:.leading){
+                    if contraseña.isEmpty{
+                        Text("Confirma Contraseña").font(.caption).foregroundColor(Color("Light-Gray"))
+                    }
+                    SecureField("", text: $confirmarcontraseña).foregroundColor(.white)
+                }
+                Divider().frame(height:1).background(Color("Dark-Cian")).padding(.bottom)
+            }
+                Button(action: IniciarSesion, label: {
+                    Text("REGISTRATE").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white)
+                        .frame(maxWidth:.infinity,alignment: .center).padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18)).overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color("Dark-Cian"),lineWidth: 1).shadow(color: .white,radius: 1))
+                })
+                
+                Text("Iniciar Sesion con Redes Sociales").foregroundColor(.white).frame(maxWidth:.infinity, alignment:.center).foregroundColor(.white)
+                HStack{
+                    Button(action: {print("Inicio de sesion con Facebook")}, label: {
+                        Text("Facebook")
+                            .font(.subheadline)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                            .padding(.vertical,3.0)
+                            .frame(maxWidth:.infinity,alignment: .center).background(Color("Blue-Gray"))
+                            .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                    })
+                    .padding()
+                    Button(action: {print("Inicio de sesion con Twitter")}, label: {
+                        Text("Twitter").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                            .frame(maxWidth:.infinity,alignment: .center).background(Color("Blue-Gray"))
+                            .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                    })
+                    
+                }
+                
+                
+            }.padding(.horizontal,77.0)
+            
+        }
+    }
+}
+
+
+func TomarFoto(){
+    print("Voy a tomar fotografia de perfil")
 }
 
 struct ContentView_Previews: PreviewProvider {
